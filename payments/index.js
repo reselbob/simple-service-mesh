4c4d41ab31e45d720f0d6bbb9c61bd85f2974b20
Bob Reselman <bob@CogArtTech.com>
2022-11-30 17:31:57 -0800

    adding debugging information

--- payments/index.js
+++ payments/index.js
@@ -58,35 +58,16 @@ app.get('/', async(req, res) => {
     res.status(200).send(payments);
 });
 
-const validations = [
-    'creditCard.number',
-    'creditCard.expirationDate',
-    'creditCard.cvv',
-    'creditCard.cardHolder.id',
-    'creditCard.cardHolder.id',
-    'creditCard.cardHolder.firstName',
-    'creditCard.cardHolder.lastName',
-    'creditCard.cardHolder.email',
-    'customer.id',
-    'customer.firstName',
-    'customer.lastName',
-    'customer.email',
-    'product.id',
-    'product.category',
-    'product.description',
-    'product.price',
-    'purchaseDate'
-];
-
 /**
  * Poor man's data validation. If only I had more time...
  * @param data
  */
 const validatePostData = (data) => {
+    logger.info(`validating ${JSON.stringify(data)}`)
     const errors = [];
-    if(!data.creditCard) errors.push("Missing credit card data\n");
-    if(!data.product) errors.push("Missing product data\n");
-    if(!data.customer) errors.push("Missing customer data\n");
+    if(!data.creditCard) errors.push("Missing credit card data");
+    if(!data.product) errors.push("Missing product data");
+    if(!data.customer) errors.push("Missing customer data");
 
     if(errors.length > 0){
         throw new Error(`There are problems with the order's data: ${JSON.stringify(errors)}`)
@@ -98,7 +79,7 @@ app.post('/', async(req, res) => {
     try {
         validatePostData(data);
     } catch (e) {
-        res.status(422).send({status: 422, message: e.message});
+        res.status(422).send({status: 422, message: e.message, data: JSON.stringify(data) });
         return;
     }
     data.id = uuidv4();
